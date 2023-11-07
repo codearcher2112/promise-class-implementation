a83dbffaf12f5666887457202d6faabf5fb76751
codearcher2112 <haykak5@gmail.com>
2023-11-07 17:11:29 +0400

    Feature ( Promise Class Implementation - Then ) Implement then and multiple thens for same promise

--- promiseClassImplementation.js
+++ promiseClassImplementation.js
@@ -1,4 +1,15 @@
+const STATE = {
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected',
+    PENDING: 'pending',
+}
+
 class MyPromise {
+    #thenCbs = []
+    #catchCbs = []
+    #state = STATE.PENDING
+    #value
+
     constructor(cb) {
         try {
             cb(this.#onSuccess, this.#onFail);
@@ -7,11 +18,41 @@ class MyPromise {
         }
     }
 
-    #onSuccess(value) {
+    #runCallbacks() {
+        if (this.#state === STATE.FULFILLED) {
+            this.#thenCbs.forEach(callback => {
+                callback(this.#value);
+            });
 
+            this.#thenCbs = [];
+        }
+
+        if (this.#state === STATE.REJECTED) {
+            this.#catchCbs.forEach(callback => {
+                callback(this.#value);
+            });
+
+            this.#catchCbs = [];
+        }
+    }
+
+    #onSuccess(value) {
+        if (this.#state !== STATE.PENDING) return;
+        this.#value = value;
+        this.#state = STATE.FULFILLED;
+        this.#runCallbacks;
     }
 
     #onFail(value) {
+        if (this.#state !== STATE.PENDING) return;
+        this.#value = value;
+        this.#state = STATE.REJECTED;
+        this.#runCallbacks;
+    }
 
+    then(cb) {
+        this.#thenCbs.push(cb);
+
+        this.#runCallbacks();
     }
 }
