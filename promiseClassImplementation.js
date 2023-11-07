e6db5c20d0fe21d122430f6606a2a7648b90f36f
codearcher2112 <haykak5@gmail.com>
2023-11-07 17:57:33 +0400

    Feature ( Promise Class Implementation - Finally ) Implement finally

--- promiseClassImplementation.js
+++ promiseClassImplementation.js
@@ -103,4 +103,17 @@ class MyPromise {
     catch(cb) {
         return this.then(undefined, cb);
     }
+
+    finally(cb) {
+        return this.then(
+            result => {
+                cb();
+                return result;
+            }, 
+            result => {
+                cb();
+                throw result;
+            }
+        );
+    }
 }
